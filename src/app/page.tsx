a01e5cbe0bd8c63fee00d1438d288251a5608136
dammar093 <dammarrana093@gmail.com>
2024-10-06 17:26:56 +0545

    add login if user is not logged in then redirect to the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export const Home = () => {
-  return <div>Home</div>;
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/Navbar";
+
+const Home = () => {
+  const router = useRouter();
+
+  const user = null;
+  if (!user) {
+    return router.push("/login");
+  }
+
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
+
+export default Home;
