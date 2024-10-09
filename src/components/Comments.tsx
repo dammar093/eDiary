8bab19a5f5ca6fd4724d7b19e57d22d83f4a6e2f
dammar093 <dammarrana093@gmail.com>
2024-10-09 09:00:58 +0545

    add comment cpmponent

---
+++ src/components/Comments.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "./Profile";
+
+const Comments = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <Profile />
+      <span className="text-gray-600 text-sm">Nice❤️</span>
+    </div>
+  );
+};
+
+export default Comments;
